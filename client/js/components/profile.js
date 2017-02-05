162281d1cfe189a4b48309bb22426324dfc2d342
Diya Zhao <zdy.gogo@msn.com>
2017-02-04 17:30:14 -0800

    modal works but need stylish

--- client/js/components/profile.js
+++ client/js/components/profile.js
@@ -1,32 +1,76 @@
 var React = require('react');
 var MovieList = require('./movie-list');
-import { Tooltip, OverlayTrigger, ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
-
+// var ReactBootstrap = require('react-bootstrap');
+// var Modal = ReactBootstrap.Modal;
+//import { Tooltip, OverlayTrigger, ButtonToolbar, DropdownButton, MenuItem} from 'react-bootstrap';
+//var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+var Modal = require('react-modal');
 var Profile = React.createClass({
+
+
+
+
 	getInitialState: function() {
 		return {show: false}
 	},
 
-	retrieveMovieList: function() {
+	_openModal: function() {
 		this.setState({show: true})
 	},
 
 	render: function() {
+
+	var customStyle={
+  overlay : {
+    position          : 'fixed',
+    top               : 0,
+    left              : 0,
+    right             : 0,
+    bottom            : 0,
+    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+  },
+  content : {
+    position                   : 'absolute',
+    top                        : '40px',
+    left                       : '40px',
+    right                      : '40px',
+    bottom                     : '40px',
+    border                     : '1px solid #ccc',
+    background                 : 'red',
+    overflow                   : 'auto',
+    WebkitOverflowScrolling    : 'touch',
+    borderRadius               : '4px',
+    outline                    : 'none',
+    padding                    : '20px'
+ 
+  }
+}
+
+
 		var movieList = this.props.movieList;
 		//{this.state.show ? <MovieList movieList={movieList} /> : null}
-		// console.log(this.props.profileName)
+		console.log(this.props.profileName)
 		// console.log(this.props.movieList)
 		return (
 			<div className="profile"> 
+				<button type="button" onClick={this._openModal}> {this.props.profileName} </button> 
+			<Modal style={customStyle}
+  isOpen={this.state.show}
+  
+  
+  contentLabel="Modal"
+>
+  <h1>Modal Content</h1>
+  <p>Etc.</p>
+</Modal>
+        
 
-				<DropdownButton type="button" onClick={this.retrieveMovieList} title={this.props.profileName} id="profileButton">
-					<MenuItem eventKey="1"> <MovieList movieList={movieList} /> </MenuItem>
-					
-				</DropdownButton>
-				
 		    </div>
 		)
 	}
 })
+
+
+
 
 module.exports = Profile;
