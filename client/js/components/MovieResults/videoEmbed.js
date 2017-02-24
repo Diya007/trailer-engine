0baf51c567964ef8ce08cbb9421a8e0d95e2f615
Diya Zhao <zdy.gogo@msn.com>
2017-02-24 14:13:05 -0800

    refactor code

--- client/js/components/MovieResults/videoEmbed.js
+++ client/js/components/MovieResults/videoEmbed.js
@@ -4,7 +4,9 @@ var ReactPlayer = require("react-player");
 var PlayVideo = React.createClass({
 	render: function() {
 		return (
+			
 			<ReactPlayer className="player"  url={this.props.url}  height={200} playing />
+			
 		)
 	}
 
